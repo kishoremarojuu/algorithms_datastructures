d50bf6bff2c38d2c24a5551ba3808b1e2e14c890
kishoremarojuu <kishore.maraju@thermofisher.com>
2021-01-24 20:48:47 -0600

    first commit

---
+++ src/main/java/com/algo_ds/algorithms_datastructures1/AlgorithmsDatastructures1Application.java
@@ -0,0 +1,13 @@
+package com.algo_ds.algorithms_datastructures1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmsDatastructures1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgorithmsDatastructures1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/algo_ds/algorithms_datastructures1/AlgorithmsDatastructures1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algo_ds.algorithms_datastructures1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgorithmsDatastructures1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
