cae43249eb675c50de692fb1c581ac872ab23278
kishoremarojuu <kishore.maraju@thermofisher.com>
2021-01-24 20:58:47 -0600

    add algo & ds programms

---
+++ src/main/java/com/algo_ds/algorithms_datastructures1/iterations_on_ds/list/SortingList.java
@@ -0,0 +1,22 @@
+package com.algo_ds.algorithms_datastructures1.iterations_on_ds.list;
+
+import java.util.ArrayList;
+
+public class SortingList {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> arrlist = new ArrayList<Integer>();
+        arrlist.add(14);
+        arrlist.add(7);
+        arrlist.add(39);
+        arrlist.add(40);
+
+        //looping using java8 streams
+        arrlist.stream()
+                .sorted()
+                .forEach(System.out::println);
+
+
+
+    }
+}
